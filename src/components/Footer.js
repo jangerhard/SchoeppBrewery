d5f20c2c9bb1255c81ce8841854e8d0db2717db1
jangerhard <jgschoepp@gmail.com>
2018-04-12 21:17:18 +0200

    Fix layouts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,12 @@ import React from 'react'
 import style from './Footer.module.css'
 
 const Footer = ({creator, link}) => (
-
+<div className={style.background}>
     <div className={style.container}>
         Created by
         <a href={link}>{creator}</a>
     </div>
+</div>
 );
 
 export default Footer;
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,14 +3,15 @@ import style from './navigation.module.css'
 import Link from 'gatsby-link';
 
 const NavigationHeader = () => (
-
-    <div className={style.container}>
-        <Link to={"/habadenero/"} className={style.link} data-ripple="true">
-            Haba DeNero
-        </Link>
-        <Link to={"/lillemy/"} className={style.link} data-ripple="true">
-            Lille My
-        </Link>
+    <div className={style.background}>
+        <div className={style.container}>
+            <Link to={"/habadenero/"} className={style.link} data-ripple="true">
+                Haba DeNero
+            </Link>
+            <Link to={"/lillemy/"} className={style.link} data-ripple="true">
+                Lille My
+            </Link>
+        </div>
     </div>
 );
 
