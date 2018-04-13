3f7f2d3d4c9adac8dedd95aec32bb3e0405b3fa8
jangerhard <jgschoepp@gmail.com>
2018-04-13 22:37:38 +0200

    Introduce CSS Grid

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,49 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import styles from './index.module.css';
 
 import Header from '../components/header'
 import NavigationHeader from "../components/navigation";
 import Footer from "../components/Footer";
 
 const Layout = ({children, data}) => (
-    <div>
+    <div className={styles.body}>
         <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-                {name: 'description', content: 'A fake website showcasing real hot sauces and semi-real beers'},
-                {name: 'keywords', content: 'hotsauce, schøpp, brewery, homebrew, hot, sauce'},
+                {
+                    name: 'description',
+                    content: 'A fake website showcasing real hot sauces and semi-real beers'
+                },
+                {
+                    name: 'keywords',
+                    content: 'hotsauce, schøpp, brewery, homebrew, hot, sauce'
+                },
             ]}
         />
-        <Header siteTitle={data.site.siteMetadata.title}/>
-        <NavigationHeader/>
-        <div
-            style={{
-                margin: '0 auto',
-                maxWidth: 960,
-                padding: '1.0875rem 1.45rem',
-            }}
-        >
-            {children()}
+
+        <div className={styles.wrapper}>
+
+            <div className={styles.header}>
+                <Header siteTitle={data.site.siteMetadata.title}/>
+            </div>
+
+            <div className={styles.nav}>
+                <NavigationHeader/>
+            </div>
+
+            <div className={styles.content}>
+                {children()}
+            </div>
+
+            <div className={styles.footer}>
+                <Footer
+                    creator={data.site.siteMetadata.creator}
+                    link={data.site.siteMetadata.homepage}
+                />
+            </div>
         </div>
-        <Footer
-            creator={data.site.siteMetadata.creator}
-            link={data.site.siteMetadata.homepage}
-        />
     </div>
 );
 
