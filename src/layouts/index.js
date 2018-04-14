31e70f43edb8b6e472ebffb14d6f03aa2fca0e24
Jan Schøpp <jan.gerhard.schopp@nordea.no>
2018-04-14 13:40:44 +0200

    Change to image

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -30,7 +30,7 @@ const Layout = ({children, data}) => (
             </div>
 
             <div className={styles.image}>
-                <div style={{
+                <image style={{
                     backgroundImage: "url(https://res.cloudinary.com/simpleview/image/upload/c_fill,f_auto,h_575,q_64,w_1200/v1/clients/norway/lofthus-fjord-norway-2-1_4050a7a3-def7-4791-a010-f250f3074faa.jpg)",
                     width: "100%",
                     height: "auto"
